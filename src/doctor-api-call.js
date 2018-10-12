55637294dc8d1e801ede60bb51f50aa528b5a399
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-10-12 14:42:59 -0700

    add frontend logic

--- src/doctor-api-call.js
+++ src/doctor-api-call.js
@@ -1,8 +1,8 @@
-export default function doctorAPICall(state, city, name = '', specialty = '') {
+export default function doctorAPICall(state, city, name = '', issue = '') {
 
     let doctorPromise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?user_key=${ process.env.exports.apiKey }&name=${ name }&specialty_uid=${ specialty }&location=${ state }-${ city }`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?user_key=${ process.env.exports.apiKey }&name=${ name }&query=${ issue }&location=${ state }-${ city }`;
       doctorRequest.onload = () => {
         if (this.status === 200) {
           resolve(doctorRequest.response);
@@ -17,19 +17,19 @@ export default function doctorAPICall(state, city, name = '', specialty = '') {
     doctorPromise.then((response) => {
       const body = JSON.parse(response);
       const names = [];
-      const specialties = [];
+      const issues = [];
       const dataLocation = body.data;
       const profileLocation = body.data.profile;
-      const specialtiesLocation = body.data.profile.specialties;
+      const issuesLocation = body.data.profile.issues;
 
       for (let i = 0; i < dataLocation.length; i ++) {
         names.push(profileLocation.first_name, profileLocation.last_name, profileLocation.title);
-        for (let i = 0; i < specialtiesLocation.length; i ++) {
-          specialties.push(specialtiesLocation.actor, specialtiesLocation.description)
+        for (let i = 0; i < issuesLocation.length; i ++) {
+          issues.push(issuesLocation.actor, issuesLocation.description)
         };
       };
 
-      const parsedResults = [names, specialties];
+      const parsedResults = [names, issues];
       return parsedResults;
 
     }, (error) => {
--- src/index.js
+++ src/index.js
@@ -6,23 +6,49 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 $(document).ready(() => {
   $('#').submit(function() {
     event.preventDefault();
-    let nameInput = $('#').val();
-    $('#').val("");
-    let specialtyInput = $('#').val();
-    $('#').val("");
-    let stateInput = $('#').val();
-    $('#').val("");
-    let cityInput = $('#').val();
-    $('#').val("");
+    let nameInput = $('#doctor-name').val();
+    $('#doctor-name').val("");
+    let issueInput = $('#medical-issue').val();
+    $('#medical-issue').val("");
+    let stateInput = $('#state').val();
+    $('#state').val("");
+    let cityInput = $('#city').val();
+    $('#city').val("");
 
-    const results = doctorAPICall(stateInput, cityInput, nameInput, specialtyInput);
+    const results = doctorAPICall(stateInput, cityInput, nameInput, issueInput);
     console.log(results);
 
     // results comes back as: [names = [[first, last, title], [first, last, title], ...], specialties = [[actor, description], [actor, description], ...];
 
     if (typeof results === 'array') { //success
-      $('#').text(``);
-      $('#').text(``);
+      const names = results[0];
+      const specialties = results[1];
+      const returnResults = (namesArray, specialtiesArray) => {
+        let listItemName = () => {
+          for (i = 0; i < namesArray.length; i ++) {
+            for (var i = 0; i < nameArray.length; i++) {
+              let nameAndTitle = `${ nameArray[0] } ${ nameArray[2] }, ${ nameArray[3] }`;
+              return nameAndTitle;
+            }
+          }
+        };
+        let listItemSpecialty = () => {
+          for (i = 0; i < specialtiesArray.length; i ++) {
+            for (var i = 0; i < specialtyArray.length; i++) {
+              let specialtySubList = `<li>${ specialtyArray[0] }</li><li>${ specialtyArray[1] }</li>`;
+              return specialtySubList;
+            }
+          }
+        };
+        const finalArray = [listItemName(), listItemSpecialty()];
+        return finalArray;
+      };
+
+      const outputToUserArray = returnResults(names, specialties);
+
+
+      $('#results').append(`<li>${ outputToUserArray[0] }</li>
+        <ul>${ outputToUserArray[1] }</ul>`);
     } else { //error
       $('#').text(`There was an error processing your request: ${results}
         Please try again.`);
